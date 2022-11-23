d6b3abc72c55c0d2db1e71f2126dc2592d094326
PaolaPiaPalumbo <paolapia.palumbo@mail.polimi.it>
2022-11-23 12:30:46 +0100

    added websocket

---
+++ public/sketch.js
@@ -0,0 +1,16 @@
+let clientSocket = io();
+
+clientSocket.on("connect", newConnection)
+
+function newConnection(){
+  console.log(clientSocket.id);
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(mouseX, mouseY, 20);
+}
--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("ciao");
+console.log("scripted loaded");
+
+let express = require("express"); //load express module
+
+let app = express(); //create a web application: active express and put it in the variable app
+
+let port = 3000;
+
+let server = app.listen(port); //tell express to wait for the input from the port 3000
+
+app.use(express.static("public"));//subfunction of express that make run the folder "public"
+
+console.log("running server on http://localhost:"+port);
+
+let serverSocket = require("socket.io"); //load socket module
+
+let io = serverSocket(server); //io is the connection point between the two server service
+
+io.on("connection", newConnection);
+
+function newConnection(newSocket){
+  console.log(newSocket);
+}
